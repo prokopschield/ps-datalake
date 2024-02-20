f329319db362c9ecc787d41bba5e7198d0011a7b
Prokop <prokop.schield@protonmail.com>
2024-02-20 11:14:09 +0100

    feat(src/helpers/compressor.rs): implement decompress()

--- src/helpers/compressor.rs
+++ src/helpers/compressor.rs
@@ -24,30 +24,72 @@ pub fn put_compressor(
     cell.replace(Some(compressor));
 }
 
+pub fn alloc_decompressor() -> libdeflater::Decompressor {
+    libdeflater::Decompressor::new()
+}
+
+pub fn get_decompressor(
+    cell: &RefCell<Option<libdeflater::Decompressor>>,
+) -> libdeflater::Decompressor {
+    match cell.replace(None) {
+        Some(compressor) => compressor,
+        None => alloc_decompressor(),
+    }
+}
+
+pub fn put_decompressor(
+    cell: &RefCell<Option<libdeflater::Decompressor>>,
+    decompressor: libdeflater::Decompressor,
+) {
+    cell.replace(Some(decompressor));
+}
+
 pub struct Compressor {
-    cell: RefCell<Option<libdeflater::Compressor>>,
+    compressor: RefCell<Option<libdeflater::Compressor>>,
+    decompressor: RefCell<Option<libdeflater::Decompressor>>,
 }
 
 impl Compressor {
     pub fn new() -> Self {
         Self {
-            cell: RefCell::from(Some(alloc_compressor())),
+            compressor: RefCell::from(Some(alloc_compressor())),
+            decompressor: RefCell::from(Some(alloc_decompressor())),
         }
     }
 
     pub fn compress(&self, data: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
-        let mut compressor = get_compressor(&self.cell);
+        let mut compressor = get_compressor(&self.compressor);
         let mut out_data = Vec::with_capacity(COMPRESSION_VEC_CAPACITY);
 
         unsafe { out_data.set_len(COMPRESSION_VEC_CAPACITY) };
 
         let result = compressor.deflate_compress(data, out_data.as_mut_slice());
 
-        put_compressor(&self.cell, compressor);
+        put_compressor(&self.compressor, compressor);
 
         let size = result?;
 
         if size < COMPRESSION_VEC_CAPACITY {
+            unsafe { out_data.set_len(size) };
+        }
+
+        Ok(out_data)
+    }
+
+    pub fn decompress(&self, data: &[u8], out_size: usize) -> Result<Vec<u8>, Box<dyn Error>> {
+        let mut out_data = Vec::with_capacity(out_size);
+
+        unsafe { out_data.set_len(out_size) };
+
+        let mut decompressor = get_decompressor(&self.decompressor);
+
+        let result = decompressor.deflate_decompress(data, out_data.as_mut_slice());
+
+        put_decompressor(&self.decompressor, decompressor);
+
+        let size = result?;
+
+        if size < out_size {
             unsafe { out_data.set_len(size) };
         }
 
