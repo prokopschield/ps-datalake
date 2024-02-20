c49192940a06b5e2b1b8265f59d843efdb6c52de
Prokop <prokop.schield@protonmail.com>
2024-02-20 12:32:27 +0100

    refactor(src/helpers/cypher.rs): use parse_key()

--- src/helpers/cypher.rs
+++ src/helpers/cypher.rs
@@ -28,11 +28,9 @@ pub fn parse_key(key: &[u8]) -> ([u8; KSIZE], [u8; NSIZE]) {
 pub fn encrypt(data: &[u8], compressor: Compressor) -> Result<Encrypted, Box<dyn Error>> {
     let compressed_data = compressor.compress(data)?;
     let hash_of_raw_data = ps_hash::hash(data);
-    let binary_hash_of_raw_data = ps_base64::decode(hash_of_raw_data.as_bytes());
-    let encryption_key = &binary_hash_of_raw_data[0..32];
-    let nonce = &binary_hash_of_raw_data[32..35];
-    let chacha = chacha20poly1305::ChaCha12Poly1305::new(encryption_key.into());
-    let encrypted_data = chacha.encrypt(nonce.into(), compressed_data.as_ref())?;
+    let (encryption_key, nonce) = parse_key(&hash_of_raw_data.as_bytes());
+    let chacha = chacha20poly1305::ChaCha12Poly1305::new(&encryption_key.into());
+    let encrypted_data = chacha.encrypt(&nonce.into(), compressed_data.as_ref())?;
     let hash_of_encrypted_data = ps_hash::hash(&encrypted_data);
 
     Ok(Encrypted {
