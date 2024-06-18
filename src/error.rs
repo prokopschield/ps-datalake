018e22f4e7f0505356fb9db407845ed3515bcf48
Prokop <prokop.schield@protonmail.com>
2024-06-19 01:54:46 +0200

    chore(src/error.rs): add RangeError

--- src/error.rs
+++ src/error.rs
@@ -6,4 +6,6 @@ pub enum PsDataLakeError {
     IoError(#[from] std::io::Error),
     #[error(transparent)]
     PsDataChunkError(#[from] ps_datachunk::PsDataChunkError),
+    #[error("Index out of range")]
+    RangeError,
 }
