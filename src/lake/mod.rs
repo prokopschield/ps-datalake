74ccbb2ce783b01f1cffa66bff9cfb9580785ec6
Prokop <prokop.schield@protonmail.com>
2024-07-06 16:45:15 +0200

    feat(src/lake): declare DataLake

---
+++ src/lake/mod.rs
@@ -0,0 +1,11 @@
+use crate::store::DataStore;
+
+#[derive(Clone, Default)]
+pub struct DataLakeStores<'lt> {
+    pub readable: Vec<DataStore<'lt>>,
+    pub writeable: Vec<DataStore<'lt>>,
+}
+
+pub struct DataLake<'lt> {
+    pub stores: DataLakeStores<'lt>,
+}
