a3494c06693315142c347749033490b0dd750c7f
Prokop <prokop.schield@protonmail.com>
2024-07-07 15:57:23 +0200

    refactor(src/lake): use hkey resolver

--- src/lake/mod.rs
+++ src/lake/mod.rs
@@ -2,13 +2,9 @@ pub mod config;
 use crate::error::PsDataLakeError;
 use crate::error::Result;
 use crate::store::DataStore;
-use ps_datachunk::Compressor;
 use ps_datachunk::DataChunk;
 use ps_datachunk::MbufDataChunk;
-use ps_datachunk::OwnedDataChunk;
 use ps_hkey::Hkey;
-use rayon::iter::IntoParallelIterator;
-use rayon::iter::ParallelIterator;
 
 #[derive(Clone, Default)]
 pub struct DataLakeStores<'lt> {
@@ -37,57 +33,10 @@ impl<'lt> DataLake<'lt> {
         Err(error)
     }
 
-    pub fn get_chunk_by_hkey(&'lt self, hkey: &Hkey, compressor: &Compressor) -> Result<DataChunk> {
-        let chunk: DataChunk = match hkey {
-            Hkey::Raw(raw) => OwnedDataChunk::from_data_ref(raw).into(),
-            Hkey::Base64(base64) => {
-                OwnedDataChunk::from_data(ps_base64::decode(base64.as_bytes())).into()
-            }
-            Hkey::Direct(hash) => {
-                for store in &self.stores.readable {
-                    match store.get_chunk_by_hash(hash.as_bytes()) {
-                        Ok(chunk) => return Ok(chunk.into()),
-                        _ => (),
-                    }
-                }
-                Err(PsDataLakeError::NotFound)?
-            }
-            Hkey::Encrypted(_, _) => {
-                for store in &self.stores.readable {
-                    match store.get_chunk_by_hkey(hkey, compressor) {
-                        Ok(chunk) => return Ok(chunk),
-                        _ => (),
-                    }
-                }
-                Err(PsDataLakeError::NotFound)?
-            }
-            Hkey::ListRef(hash, key) => self.get_chunk_by_hkey(
-                &(self.get_chunk_by_hkey(&(hash.clone(), key.clone()).into(), compressor)?)
-                    .data_ref()
-                    .into(),
-                compressor,
-            )?,
-            Hkey::List(list) => {
-                // Parallel fetching of chunks
-                let chunks: Vec<Result<DataChunk>> = list
-                    .into_par_iter()
-                    .map(|hkey| self.get_chunk_by_hkey(hkey, &Compressor::new()))
-                    .collect();
-
-                // Collect and concatenate data from chunks
-                let buffer: Result<Vec<u8>> =
-                    chunks.into_iter().try_fold(vec![], |mut buffer, chunk| {
-                        buffer.extend_from_slice(chunk?.data_ref());
-                        Ok(buffer)
-                    });
-
-                // Convert the concatenated data into an OwnedDataChunk
-                let chunk = OwnedDataChunk::from_data(buffer?);
-
-                chunk.into()
-            }
-        };
-
-        Ok(chunk.into())
+    pub fn get_chunk_by_hkey(&'lt self, hkey: &Hkey) -> Result<DataChunk> {
+        hkey.resolve(&|hash| match self.get_encrypted_chunk(hash.as_bytes()) {
+            Ok(chunk) => Ok(chunk.into()),
+            Err(err) => Err(err),
+        })
     }
 }
