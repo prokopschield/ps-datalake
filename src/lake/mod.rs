9e84e78c135547a1475438f5b1e90e357eaad51b
Prokop <prokop.schield@protonmail.com>
2024-07-07 11:01:58 +0200

    fix(src/lake): fix typo

--- src/lake/mod.rs
+++ src/lake/mod.rs
@@ -4,7 +4,7 @@ use crate::store::DataStore;
 #[derive(Clone, Default)]
 pub struct DataLakeStores<'lt> {
     pub readable: Vec<DataStore<'lt>>,
-    pub writeable: Vec<DataStore<'lt>>,
+    pub writable: Vec<DataStore<'lt>>,
 }
 
 pub struct DataLake<'lt> {
